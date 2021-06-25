d53dcd86bdc8faed396ec6b361cedc80a0290b17
sebastianrosenzweig <sebastian.rosenzweig@fau.de>
2021-06-25 19:07:28 +0200

    fixed header

--- test/test_matlab.m
+++ test/test_matlab.m
@@ -1,60 +1,6 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Name: demoTSMtoolbox
-% Date: 12-2013
-% Programmer: Jonathan Driedger
-% http://www.audiolabs-erlangen.de/resources/MIR/TSMtoolbox/
-%
-% This is the demo script which illustrates the main functionalities of the
-% 'TSM toolbox'. For a detailed description we refer to  [DM14] (see
-% References below).
-%
-% The script proceeds in the following steps:
-%   1. It loads a wav file.
-%   2. It applies different TSM algorithms to the loaded signal with a
-%      fixed constant stretching factor.
-%   3. It visualizes the TSM results.
-%   4. It writes the computed TSM results as wav files to the hard drive.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Reference:
-% If you use the 'TSM toolbox' please refer to:
-% [DM14] Jonathan Driedger, Meinard Mueller
-%        TSM Toolbox: MATLAB Implementations of Time-Scale Modification
-%        Algorithms
-%        Proceedings of the 17th International Conference on Digital Audio
-%        Effects, Erlangen, Germany, 2014.
-%
-% License:
-% This file is part of 'TSM toolbox'.
-%
-% MIT License
-% 
-% Copyright (c) 2021 Jonathan Driedger, Meinard Mueller, International Audio
-% Laboratories Erlangen, Germany.
-% 
-% We thank the German Research Foundation (DFG) for various research grants 
-% that allow us for conducting fundamental research in music processing.
-% The International Audio Laboratories Erlangen are a joint institution of 
-% the Friedrich-Alexander-Universitaet Erlangen-Nuernberg (FAU) and
-% Fraunhofer Institute for Integrated Circuits IIS.
-% 
-% Permission is hereby granted, free of charge, to any person obtaining a 
-% copy of this software and associated documentation files (the 
-% "Software"), to deal in the Software without restriction, including 
-% without limitation the rights to use, copy, modify, merge, publish, 
-% distribute, sublicense, and/or sell copies of the Software, and to permit 
-% persons to whom the Software is furnished to do so, subject to the 
-% following conditions:
-% 
-% The above copyright notice and this permission notice shall be included 
-% in all copies or substantial portions of the Software.
-% 
-% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
-% OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
-% MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
-% IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
-% CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
-% TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
-% SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+% Test script for executing Matlab functions
+% Contributors: Sebastian Rosenzweig, Simon Schwaer, Jonathan Driedger, Meinard Mueller
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -151,5 +97,3 @@ ySpecEnv = modifySpectralEnvelope(x, x);
 
 % save all variables
 save([pathOutput 'matlab.mat'], 'x', 'window', 'Y', 'f', 't', 'xI', 'yOLA', 'yWSOLA', 'yPV', 'yPVpl', 'yHP', 'xHarm', 'xPerc', 'yPvInt', 'yTwoStep', 'yPitchShift', 'ySpecEnv', 'ySync');
-
-
